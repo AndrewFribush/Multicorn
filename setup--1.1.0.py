8986c51caa0490606516c14f10c41eab482f6daa
Andrew <andrewfribush@gmail.com>
2015-04-02 17:14:59 -0400

    Initial Commit

---
+++ setup--1.1.0.py
@@ -0,0 +1,27 @@
+import subprocess
+from setuptools import setup, find_packages, Extension
+
+# hum... borrowed from psycopg2
+def get_pg_config(kind, pg_config="pg_config"):
+    r = subprocess.check_output([pg_config, '--%s' % kind])
+    r = r.strip().decode('utf8')
+    if not r:
+        raise Warning(p[2].readline())
+    return r
+
+include_dirs = [get_pg_config(d) for d in ("includedir", "pkgincludedir", "includedir-server")]
+
+multicorn_utils_module = Extension('multicorn._utils',
+        include_dirs=include_dirs,
+        extra_compile_args = ['-shared'],
+        sources=['src/utils.c'])
+
+setup(
+ name='multicorn',
+ version='1.1.0-dev',
+ author='Kozea',
+ license='Postgresql',
+ package_dir={'': 'python'},
+ packages=['multicorn', 'multicorn.fsfdw'],
+ ext_modules = [multicorn_utils_module]
+)
